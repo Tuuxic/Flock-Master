c30e726adb0c444d71a3d92a7bd489363fcca0f4
Jonathan Ehwald <github@ehwald.info>
2023-06-30 16:26:19 +0200

    Add flashlight and thief controller

---
+++ DigitaleSpiele/Assets/Elitewerk/Scripts/ThiefController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class ThiefController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 12f;
+    public float dashForce = 30f;
+    public float dashCooldown = 2f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+    private float lastDashTimestamp;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Check if the player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+
+        // Inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Movement
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + movement);
+
+        // Rotation
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 lookDirection = hit.point - rb.transform.position;
+            lookDirection.y = 0f;
+            if (lookDirection != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+                rb.MoveRotation(targetRotation);
+            }
+        }
+
+        // Jumping
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        // Dashing
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (lastDashTimestamp + dashCooldown > Time.time) return;
+            rb.AddForce(rb.transform.forward * dashForce, ForceMode.Impulse);
+            lastDashTimestamp = Time.time;
+        }
+    }
+}
