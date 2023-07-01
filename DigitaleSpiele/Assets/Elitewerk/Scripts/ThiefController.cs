7c8fd19679fadf1336479b8ccb76c53984b872e1
Jonathan Ehwald <github@ehwald.info>
2023-07-01 18:25:31 +0200

    Allow reaching for closest can

--- DigitaleSpiele/Assets/Elitewerk/Scripts/ThiefController.cs
+++ DigitaleSpiele/Assets/Elitewerk/Scripts/ThiefController.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Animations.Rigging;
 
 public class ThiefController : MonoBehaviour
 {
     public Animator animator;
     public float moveSpeed = 5f;
+
+    public TwoBoneIKConstraint twoBoneIKConstraint;
+    public Transform leftHandTarget;
 
     private Rigidbody rb;
 
@@ -39,5 +44,42 @@ public class ThiefController : MonoBehaviour
         {
             animator.SetTrigger("Dab");
         }
+
+
+        //twoBoneIKConstraint.weight = Mathf.Lerp(0, 1, t);
+        //t += 0.1f * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject[] cans = GameObject.FindGameObjectsWithTag("Can");
+            GameObject closestCan = null;
+
+            float minDistance = Mathf.Infinity;
+            Vector3 currentPosition = transform.position;
+
+            foreach (GameObject can in cans)
+            {
+                float distance = Vector3.Distance(can.transform.position, currentPosition);
+                if (distance < minDistance)
+                {
+                    closestCan = can;
+                    minDistance = distance;
+                }
+            }
+
+            if (closestCan != null)
+            {
+                leftHandTarget.position = closestCan.transform.position;
+                twoBoneIKConstraint.weight = 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Q pressed");
+            twoBoneIKConstraint.weight = 0;
+        }
     }
+
+    //private float t = 0f;
 }
