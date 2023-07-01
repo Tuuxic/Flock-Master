bd444b16c57d153cdc8f41549cdca87cfbc3171e
Jonathan Ehwald <github@ehwald.info>
2023-07-01 02:38:53 +0200

    Add four hours worth of third person movement script

--- DigitaleSpiele/Assets/Elitewerk/Scripts/ThiefController.cs
+++ DigitaleSpiele/Assets/Elitewerk/Scripts/ThiefController.cs
@@ -3,15 +3,8 @@ using UnityEngine;
 public class ThiefController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForce = 12f;
-    public float dashForce = 30f;
-    public float dashCooldown = 2f;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
 
     private Rigidbody rb;
-    private bool isGrounded;
-    private float lastDashTimestamp;
 
     private void Start()
     {
@@ -20,43 +13,19 @@ public class ThiefController : MonoBehaviour
 
     private void Update()
     {
-        // Check if the player is grounded
-        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
-
-        // Inputs
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         // Movement
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed * Time.deltaTime;
-        rb.MovePosition(rb.position + movement);
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 movement = (horizontalInput * Camera.main.transform.right + verticalInput * cameraForward).normalized;
+        rb.velocity = movement * moveSpeed;
 
-        // Rotation
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        // Rotate towards the movement direction
+        if (movement != Vector3.zero)
         {
-            Vector3 lookDirection = hit.point - rb.transform.position;
-            lookDirection.y = 0f;
-            if (lookDirection != Vector3.zero)
-            {
-                Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
-                rb.MoveRotation(targetRotation);
-            }
-        }
-
-        // Jumping
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-
-        // Dashing
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            if (lastDashTimestamp + dashCooldown > Time.time) return;
-            rb.AddForce(rb.transform.forward * dashForce, ForceMode.Impulse);
-            lastDashTimestamp = Time.time;
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            rb.transform.rotation = Quaternion.Lerp(rb.transform.rotation, toRotation, Time.deltaTime * 10f);
         }
     }
 }
